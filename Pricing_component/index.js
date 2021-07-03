6a259015cc0b3c0d4cf3aa1e861b143f6f1997e4
bane <bane.tepavcevic@gmail.com>
2021-07-03 23:25:44 +0200

    Added solution with three function

--- Pricing_component/index.js
+++ Pricing_component/index.js
@@ -1,22 +1,66 @@
 // 🐱 🦁 🐯
 
-// Grab elements
+/* Instructions */
+/* When clicked, the selected card should enlarge (up to you how much) and the price should have a color of #ffd60a*/
+/* The non-selected cards should reduce in opacity */
+/* Stretch goal: add a toggle to display monthly and annual prices */
 
-//const btns = document.querySelectorAll('button')
-const btn1 = document.querySelector('.btn1')
-const btn2 = document.querySelector('.btn2')
-const btn3 = document.querySelector('.btn3')
+
+const priceCat = document.querySelector('.card-cat-price')
+const priceLion = document.querySelector('.card-cat-lion')
+const priceTiger = document.querySelector('.card-tiger-price')
+
+const cardCat = document.querySelector('.card-cat')
+const cardLion = document.querySelector('.card-lion')
+const cardTiger = document.querySelector('.card-Tiger')
 
 // Event listener
 
-btn1.addEventListener('click', clickPrice)
-btn2.addEventListener('click', clickPrice)
-btn3.addEventListener('click', clickPrice)
+cardCat.addEventListener('click', clickCardCat)
+cardLion.addEventListener('click', clickCardLion)
+cardTiger.addEventListener('click', clickCardTiger)
 
 
-function clickPrice() {
+function clickCardCat() {
 
-    // ako se dugme klikne uradio sledece
-    btn1.style.backbro
-    if(btn1)
+    //Resize card
+    cardCat.classList.toggle('card-animation')
+    cardLion.classList.remove('card-animation')
+    cardTiger.classList.remove('card-animation')
+    // Change color of price
+    priceCat.style.color='#ffd60a'
+    cardCat.style.opacity = 1
+    cardLion.style.opacity = .6
+    cardTiger.style.opacity = .6
 }
+
+
+function clickCardLion() {
+
+    //Resize card
+    cardLion.classList.toggle('card-animation')
+    cardCat.classList.remove('card-animation')
+    cardTiger.classList.remove('card-animation')
+    // Change color of price
+    priceCat.style.color='#fff'
+    priceLion.style.color='#ffd60a'
+    //priceTiger.style.color='#fff'
+
+    cardLion.style.opacity = 1
+    cardCat.style.opacity = .6
+    cardTiger.style.opacity = .6
+}
+
+function clickCardTiger() {
+
+    //Resize card
+    cardTiger.classList.toggle('card-animation')
+    cardCat.classList.remove('card-animation')
+    cardTiger.classList.remove('card-animation')
+    // Change color of price
+    priceTiger.style.color='#ffd60a'
+    cardTiger.style.opacity = 1
+    cardLion.style.opacity = .6
+    cardCat.style.opacity = .6
+}
+
