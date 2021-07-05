29f3945ab3832ede4efb8c23e741d05de60a577c
bane <bane.tepavcevic@gmail.com>
2021-07-05 11:13:55 +0200

    Implemented nwe approach with forEach function and filter

--- Pricing_component/index.js
+++ Pricing_component/index.js
@@ -5,7 +5,7 @@
 /* The non-selected cards should reduce in opacity */
 /* Stretch goal: add a toggle to display monthly and annual prices */
 
-
+const cards = document.querySelectorAll('.card')
 const priceCat = document.querySelector('.card-cat-price')
 const priceLion = document.querySelector('.card-cat-lion')
 const priceTiger = document.querySelector('.card-tiger-price')
@@ -16,51 +16,73 @@ const cardTiger = document.querySelector('.card-Tiger')
 
 // Event listener
 
-cardCat.addEventListener('click', clickCardCat)
-cardLion.addEventListener('click', clickCardLion)
-cardTiger.addEventListener('click', clickCardTiger)
+// cardCat.addEventListener('click', clickCardCat)
+// cardLion.addEventListener('click', clickCardLion)
+// cardTiger.addEventListener('click', clickCardTiger)
 
+cards.forEach(function(card) {
+    // add eventListener
+    card.addEventListener('click', selectCard)
+})
 
-function clickCardCat() {
+function selectCard() {
+   
+   //this.classList.toggle('selected')
+  const selected = this
+ 
+  selected.classList.toggle('selected')
+  const notSelected = [...cards].filter(function(value) {
+      return value != selected
+      
+  })
+  console.log(notSelected)
 
-    //Resize card
-    cardCat.classList.toggle('card-animation')
-    cardLion.classList.remove('card-animation')
-    cardTiger.classList.remove('card-animation')
-    // Change color of price
-    priceCat.style.color='#ffd60a'
-    cardCat.style.opacity = 1
-    cardLion.style.opacity = .6
-    cardTiger.style.opacity = .6
+  notSelected.forEach(function(card) {
+      card.classList.toggle('unselected')
+      priceLion.style.color='#ffd60a'
+  })
+   
 }
+// function clickCardCat() {
+
+//     //Resize card
+//     cardCat.classList.toggle('card-animation')
+//     cardLion.classList.remove('card-animation')
+//     cardTiger.classList.remove('card-animation')
+//     // Change color of price
+//     priceCat.style.color='#ffd60a'
+//     cardCat.style.opacity = 1
+//     cardLion.style.opacity = .6
+//     cardTiger.style.opacity = .6
+// }
 
 
-function clickCardLion() {
+// function clickCardLion() {
 
-    //Resize card
-    cardLion.classList.toggle('card-animation')
-    cardCat.classList.remove('card-animation')
-    cardTiger.classList.remove('card-animation')
-    // Change color of price
-    priceCat.style.color='#fff'
-    priceLion.style.color='#ffd60a'
-    //priceTiger.style.color='#fff'
+//     //Resize card
+//     cardLion.classList.toggle('card-animation')
+//     cardCat.classList.remove('card-animation')
+//     cardTiger.classList.remove('card-animation')
+//     // Change color of price
+//     priceCat.style.color='#fff'
+//     priceLion.style.color='#ffd60a'
+//     //priceTiger.style.color='#fff'
 
-    cardLion.style.opacity = 1
-    cardCat.style.opacity = .6
-    cardTiger.style.opacity = .6
-}
+//     cardLion.style.opacity = 1
+//     cardCat.style.opacity = .6
+//     cardTiger.style.opacity = .6
+// }
 
-function clickCardTiger() {
+// function clickCardTiger() {
 
-    //Resize card
-    cardTiger.classList.toggle('card-animation')
-    cardCat.classList.remove('card-animation')
-    cardTiger.classList.remove('card-animation')
-    // Change color of price
-    priceTiger.style.color='#ffd60a'
-    cardTiger.style.opacity = 1
-    cardLion.style.opacity = .6
-    cardCat.style.opacity = .6
-}
+//     //Resize card
+//     cardTiger.classList.toggle('card-animation')
+//     cardCat.classList.remove('card-animation')
+//     cardTiger.classList.remove('card-animation')
+//     // Change color of price
+//     priceTiger.style.color='#ffd60a'
+//     cardTiger.style.opacity = 1
+//     cardLion.style.opacity = .6
+//     cardCat.style.opacity = .6
+// }
 
