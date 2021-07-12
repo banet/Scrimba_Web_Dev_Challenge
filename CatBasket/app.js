d0324085593cf3032c736a2b465aa082457d7be6
bane <bane.tepavcevic@gmail.com>
2021-07-12 19:08:19 +0200

    Add new project Cat Basket to loopw throw array and return cards

---
+++ CatBasket/app.js
@@ -0,0 +1,16 @@
+
+
+const cats = [
+    {
+        name: 'Pumpkin',
+        img: 'https://unsplash.com/photos/OzAeZPNsLXk'
+    },
+    {
+        name: 'Pumpkin',
+        img: 'https://images.unsplash.com/photo-1569591159212-b02ea8a9f239?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDR8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60'
+    },
+    {
+        name: 'Pumpkin',
+        img: 'https://images.unsplash.com/photo-1550414485-9f22b971dbf0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDE4fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60'
+    }
+]
